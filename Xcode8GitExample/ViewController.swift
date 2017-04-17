f83a4629a3fabf870e0c86b538816d333a0acb79
adel alraheem <alraheem@adels-macbook-pro-9.local>
2017-04-17 09:19:18 +0300

    Initial Commit

---
+++ Xcode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8GitExample
+//
+//  Created by adel alraheem on 4/17/17.
+//  Copyright © 2017 adel alraheem. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
